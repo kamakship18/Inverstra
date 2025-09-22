a6bf3fa0e4340ce79cf4b463c94b448b64f444d4
Kamakshi Pandoh <kamakshipandoh@Kamakshis-MacBook-Pro.local>
2025-09-22 17:39:21 +0530

    fixed deployment issue

--- frontend/pages/api/predictions.js
+++ frontend/pages/api/predictions.js
@@ -1,18 +1,37 @@
 // /pages/api/predictions.js
 import { MongoClient } from 'mongodb';
 
-// MongoDB connection URI
 const uri = process.env.MONGODB_URI;
-const client = new MongoClient(uri);
+
+let client;
+let clientPromise;
+
+if (!uri) {
+  throw new Error('Please add your Mongo URI to .env.local');
+}
+
+if (process.env.NODE_ENV === 'development') {
+
+  if (!global._mongoClientPromise) {
+    client = new MongoClient(uri);
+    global._mongoClientPromise = client.connect();
+  }
+  clientPromise = global._mongoClientPromise;
+} else {
+
+  client = new MongoClient(uri);
+  clientPromise = client.connect();
+}
 
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method Not Allowed' });
   }
 
+  let client;
   try {
     // Connect to the MongoDB cluster
-    await client.connect();
+    client = await clientPromise;
     const database = client.db('inverstra');
     const predictions = database.collection('predictions');
     
@@ -54,8 +73,5 @@ export default async function handler(req, res) {
       error: 'Error saving prediction',
       details: error.message
     });
-  } finally {
-    // Close the connection when done
-    await client.close();
   }
 }
